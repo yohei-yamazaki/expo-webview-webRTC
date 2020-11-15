28f567c7f3c8ab100272b120cd28a6bbf0c2cc0a
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2020-11-15 23:29:44 +0900

    yw expo

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { StatusBar } from 'expo-status-bar';
+
+export const App = () => {
   return (
     <View style={styles.container}>
-
-      <Text>Open
-        up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
