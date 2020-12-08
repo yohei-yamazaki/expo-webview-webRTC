c86360918c1dd1f9e8393decc3be2b08ea3a61f0
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2020-12-08 22:23:38 +0900

    add webview

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import { StatusBar } from 'expo-status-bar';
+import WebView from 'react-native-webview';
+
+import config from './config/variables.json';
 
 export const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <WebView source={{ uri: config['webRTC-client-uri'] }} />
       <StatusBar style="auto" />
     </View>
   );
